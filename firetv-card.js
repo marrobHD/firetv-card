d698feb5c89afa6a2868d971a0bbb1e0b30121a7
TechHome <marlon.otto@outlook.de>
2021-03-08 15:48:15 +0100

    Update firetv-card.js

--- firetv-card.js
+++ firetv-card.js
@@ -161,10 +161,10 @@ class FireTVCardServices extends LitElement {
           </div>
 
           ${
-            this._config.tv ||
+            this._config.tv && (
             this._config.volume_up ||
             this._config.volume_down ||
-            this._config.volume_mute
+            this._config.volume_mute )
               ? html`
                   <div class="row">
                     <ha-icon-button
